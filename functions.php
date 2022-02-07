a02a18d35c3fb1aab6e55dc63b02ce046b017fb9
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-07 16:54:17 +0000

    added successes, failure, malformed test functions

--- functions.php
+++ functions.php
@@ -1,12 +1,16 @@
 <?php
 
-function displayGameCard($allGames) {
-     $returnedHtml = '';
-    foreach($allGames as $game) {
-        $returnedHtml .= "<div class='gameCard'><h3> Game: " . $game['name'] . "</h3>" .
-            "<p> Play time: " . $game['play_time_in_mins'] . "</p>" .
-            "<p> Players: " . $game['min_number_of_players'] . " - " . $game['max_number_of_players'] . "</p>" .
-            "<P> Number of expansions: ". $game['expansions_available'] . "</p></div>";
+function displayGameCard(array $allGames): string {
+    if (count($allGames) == count($allGames, COUNT_RECURSIVE)) {
+        return 'this is not an array';
+    } else {
+        $returnedHtml = '';
+        foreach ($allGames as $game) {
+            $returnedHtml .= "<div class='gameCard'><h3> Game: " . $game['name'] . "</h3>" .
+                "<p>Play time: " . $game['play_time_in_mins'] . "</p>" .
+                "<p>Players: " . $game['min_number_of_players'] . " - " . $game['max_number_of_players'] . "</p>" .
+                "<p>Number of expansions: " . $game['expansions_available'] . "</p></div>";
+        }
+        return $returnedHtml;
     }
-    return $returnedHtml;
 }
--- tests/functions.php
+++ tests/functions.php
@@ -1,1 +1,31 @@
 <?php
+
+require '../functions.php';
+
+use PHPUnit\Framework\TestCase;
+class Functions extends TestCase
+{
+
+    public function testSuccessDisplayGameCard()
+    {
+        $input = [ ['id' => 1, 'name' => 'Catan', 'play_time_in_mins' => 120, 'expansions_available' => 1, 'max_number_of_players' => 3, 'min_number_of_players' => 4]];
+        $expectedOutput = "<div class='gameCard'><h3> Game: Catan</h3><p>Play time: 120</p><p>Players: 4 - 3</p><p>Number of expansions: 1</p></div>";
+        $actualOutput = displayGameCard($input);
+        $this->assertEquals($expectedOutput, $actualOutput);
+    }
+
+    public function testFailureDisplayGameCard()
+    {
+        $input = ['id' => 1, 'name' => 'Catan', 'play_time_in_mins' => 120, 'expansions_available' => 1, 'max_number_of_players' => 3, 'min_number_of_players' => 4];
+        $expectedOutput = 'this is not an array';
+        $actualOutput = displayGameCard($input);
+        $this->assertEquals($expectedOutput, $actualOutput);
+    }
+
+    public function testMalformedDisplayGameCard()
+    {
+        $input = 'banana';
+        $this->expectException(TypeError::class);
+        $output = displayGameCard($input);
+    }
+}
