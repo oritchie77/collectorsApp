40cf5a7c0e1dab00a1211f71e0771052f3fcf9b2
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-08 11:01:33 +0000

    Made changes as per code review

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
+require_once './functions.php';
+
 $db = new PDO('mysql:host=127.0.0.1: 3306; dbname=colletor', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT * FROM `games`');
 $query->execute();
 $allGames = $query->fetchAll();
 
-require_once './functions.php';
- $gameCard = displayGameCard($allGames);
- print_r($allGames[0]);
+
+ $gameCards = displayAllGameCards($allGames);
 ?>
 
 <html lang="en-GB">
 
 <head>
     <title>Collector App</title>
-
 </head>
 
 <body>
-    <h1>Board Games</h1>
-    <h2>Your Collection</h2>
-    <?= $gameCard ?>
+    <main>
+      <h1>Board Games</h1>
+        <h2>Your Collection</h2>
+        <div class="GameCardsContainer">
+            <?= $gameCards ?>
+        </div>
+    </main>
+
 </body>
 
 </html>
--- test/functions.php
+++ test/functions.php
@@ -6,26 +6,26 @@ use PHPUnit\Framework\TestCase;
 class Functions extends TestCase
 {
 
-    public function testSuccessDisplayGameCard()
+    public function testSuccessDisplayAllGameCards()
     {
-        $input = [ ['id' => 1, 'name' => 'Catan', 'play_time_in_mins' => 120, 'expansions_available' => 1, 'max_number_of_players' => 3, 'min_number_of_players' => 4]];
+        $input = [['id' => 1, 'name' => 'Catan', 'play_time_in_mins' => 120, 'expansions_available' => 1, 'max_number_of_players' => 3, 'min_number_of_players' => 4]];
         $expectedOutput = "<div class='gameCard'><h3> Game: Catan</h3><p>Play time: 120</p><p>Players: 4 - 3</p><p>Number of expansions: 1</p></div>";
-        $actualOutput = displayGameCard($input);
+        $actualOutput = displayAllGameCards($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
 
-    public function testFailureDisplayGameCard()
+    public function testFailureDisplayAllGameCards()
     {
         $input = ['id' => 1, 'name' => 'Catan', 'play_time_in_mins' => 120, 'expansions_available' => 1, 'max_number_of_players' => 3, 'min_number_of_players' => 4];
-        $expectedOutput = 'this is not an array';
-        $actualOutput = displayGameCard($input);
+        $expectedOutput = 'Incorrect Data type! Must be a Multidimensional array';
+        $actualOutput = displayAllGameCards($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
 
-    public function testMalformedDisplayGameCard()
+    public function testMalformedDisplayAllGameCards()
     {
         $input = 'banana';
         $this->expectException(TypeError::class);
-        $output = displayGameCard($input);
+        $output = displayAllGameCards($input);
     }
 }
